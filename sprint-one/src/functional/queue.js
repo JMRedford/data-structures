714b8beb998b5847ac63f59959af83e2f647349b
MKS-students <mks.students@hackreactor.com>
2015-06-10 15:11:38 -0500

    Completed functional implementations

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var first = 0;
+  var last = -1;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[last+1] = value;
+    last += 1;
   };
 
   someInstance.dequeue = function(){
+    if(storage.hasOwnProperty(first)){
+      var toRet = storage[first];
+      delete storage[first];
+      first += 1;
+      return toRet;
+    }
   };
 
   someInstance.size = function(){
+    return last - first + 1;
   };
 
   return someInstance;
